72eaa5082f277ac5bd2ed2cfa1818118c23bb571
Janek Winkler <janekx21@gmail.com>
2023-07-03 19:21:51 +0200

    :sparkles: gravity & death plane

--- code/player.gd
+++ code/player.gd
@@ -5,18 +5,21 @@ extends CharacterBody3D
 
 func _physics_process(delta: float) -> void:
 	var move = get_move()
-	var planar = Vector3(move.x, 0, -move.y)
-	var move_target = planar * max_speed
+	#var planar = Vector3(move.x, 0, -move.y)
+	var move_target = move * max_speed
 	var move_delta = delta * acceleration
 	
-	velocity = velocity.move_toward(move_target, move_delta / 2)
+	var planar_velocity = Vector2(velocity.x, velocity.z)
+	planar_velocity = planar_velocity.move_toward(move_target, move_delta)
+	velocity.x = planar_velocity.x
+	velocity.z = planar_velocity.y
+	velocity.y -= delta * 50
 	move_and_slide()
-	velocity = velocity.move_toward(move_target, move_delta / 2)
 
 
 func get_move() -> Vector2:
 	var x = Input.get_axis("move_left", "move_right")
-	var y = Input.get_axis("move_down", "move_up")
+	var y = Input.get_axis("move_up", "move_down")
 	return Vector2(x,y).normalized() 
 
 func extend(value: Vector2, with: float) -> Vector3:
@@ -24,5 +27,5 @@ func extend(value: Vector2, with: float) -> Vector3:
 
 
 func _on_hp_death() -> void:
-	global_position = Vector3.ZERO
+	global_position = Vector3.UP * 2
 	# todo better death
