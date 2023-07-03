9d079c8dc18669973ea9fdc3d02d710b37ab6892
Janek Winkler <janekx21@gmail.com>
2023-07-03 22:39:23 +0200

    joystick movement

--- code/player.gd
+++ code/player.gd
@@ -5,7 +5,6 @@ extends CharacterBody3D
 
 func _physics_process(delta: float) -> void:
 	var move = get_move()
-	#var planar = Vector3(move.x, 0, -move.y)
 	var move_target = move * max_speed
 	var move_delta = delta * acceleration
 	
@@ -14,13 +13,21 @@ func _physics_process(delta: float) -> void:
 	velocity.x = planar_velocity.x
 	velocity.z = planar_velocity.y
 	velocity.y -= delta * 50
+	
+	
 	move_and_slide()
 
 
 func get_move() -> Vector2:
 	var x = Input.get_axis("move_left", "move_right")
+	x += Input.get_joy_axis(0, JOY_AXIS_LEFT_X)
 	var y = Input.get_axis("move_up", "move_down")
-	return Vector2(x,y).normalized() 
+	y += Input.get_joy_axis(0, JOY_AXIS_LEFT_Y)
+	var dir = Vector2(x,y) 
+	if dir.length() > 1:
+		return dir.normalized()
+	else:
+		return dir
 
 func extend(value: Vector2, with: float) -> Vector3:
 	return Vector3(value.x, value.y, with)
