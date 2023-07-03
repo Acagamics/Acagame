4bb72f0ed86aebb7bbef2d7b53036ea96a6db99c
Janek Winkler <janekx21@gmail.com>
2023-07-03 19:01:37 +0200

    fixed door

--- code/door.gd
+++ code/door.gd
@@ -1,4 +1,10 @@
 extends Node3D
 
-func open():
-	$AnimationPlayer.play("open")
+var open = false
+
+func toggle():
+	if open:
+		$AnimationPlayer.play_backwards("open")
+	else:
+		$AnimationPlayer.play("open")
+	open = not open
